03257d355fa0de84e340fe00b218825701745583
Filipko^^ <fiilgameslp@gmail.com>
2024-06-13 11:30:49 +0200

    add final code

--- polievanie.ino
+++ polievanie.ino
@@ -1,57 +1,53 @@
 // C++ code
-//
+
 int moisture = 0;
 int pumpPin = 3;
+int bluePin = 5;
+int yellowPin = 6;
+int redPin = 7;
 void setup()
 {
   pinMode(A0, OUTPUT);
   pinMode(A1, INPUT);
   Serial.begin(9600);
-  pinMode(pumpPin, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
+//  pinMode(pumpPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+
   
 }
 
-void loop()
-{
-  // Apply power to the soil moisture sensor
+void loop() {
   digitalWrite(A0, HIGH);
-  delay(10); // Wait for 10 millisecond(s)
-  moisture = analogRead(A1);
-  // Turn off the sensor to reduce metal corrosion
-  // over time
+  delay(10);
+  moisture = 1024 - analogRead(A1);
   digitalWrite(A0, LOW);
   Serial.println(moisture);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(12, LOW);
+  digitalWrite(redPin, LOW);
+  digitalWrite(yellowPin, LOW);
+  digitalWrite(bluePin, LOW);
   analogWrite(pumpPin, 0);
-  if (moisture < 200) {
-  	int outputValue = 255 - map(moisture, 0, 1023, 0, 255);
-  	analogWrite(pumpPin, outputValue);
+
+
+
+  if( moisture < 400){
+    // turn on pump
+  	analogWrite(pumpPin, 250);
     Serial.print("Pump turned ON : ");
-    Serial.println(outputValue);
     delay(1000);
-  } else {
-    if (moisture < 400) {
-      digitalWrite(11, HIGH);
-    } else {
-      if (moisture < 600) {
-        digitalWrite(10, HIGH);
-      } else {
-        if (moisture < 800) {
-          digitalWrite(9, HIGH);
-        } else {
-          digitalWrite(8, HIGH);
-        }
-      }
+    digitalWrite(redPin, HIGH);
+  }else{
+    if (moisture < 600){
+      digitalWrite(yellowPin, HIGH);
     }
+    else{
+      digitalWrite(bluePin, HIGH);
   }
-  delay(1000); // Wait for 100 millisecond(s)
+  }
+
+  delay(2000);
+
+  
+
 }
